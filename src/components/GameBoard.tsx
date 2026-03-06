89d77db262cbe4459903a4910182686238dc1f6a
ryania <ryanjamesdominguez@gmail.com>
2026-03-06 11:41:22 -0600

    modified region line width to 1.6 from 2.2

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -523,7 +523,7 @@ function bakeOffscreen(
       [-1,  1, 2, 3],  // SW (q-1, r+1) → edge V2–V3
     ] as const
 
-    ctx.lineWidth   = 2.2
+    ctx.lineWidth   = 1.6
     ctx.lineCap     = 'round'
     ctx.strokeStyle = '#0d0800'
     ctx.globalAlpha = 0.78
